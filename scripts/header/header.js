22e63108329028bfb3839c26f34a4c9a53a91de3
GuanXin Wang <Alex0972112871@gmail.com>
2021-10-09 12:29:01 +0800

    fix path

--- scripts/header/header.js
+++ scripts/header/header.js
@@ -1,7 +1,7 @@
 document.body.innerHTML =
 `
 <div class="header">
-  <img id="header-pic" src="../assets/flinder_railway.jpg">
+  <img id="header-pic" src="assets/flinder_railway.jpg">
   <div class="header-text">
     <div><span class="title">City of Melbourne - Cafes and restaurants</span></div>
     <div><span class="title2">A guide to local tourists</span></div>
