0f5e6599ac06d6d0a04a40c2eb47a4f517e162b9
GuanXin Wang <Alex0972112871@gmail.com>
2021-10-11 16:43:40 +0800

    update20211011

--- scripts/mapbox/map.js
+++ scripts/mapbox/map.js
@@ -1,5 +1,5 @@
 const map = document.getElementById("map")
 map.innerHTML = 
 `
-<iframe width='100%' height='400px' src="https://api.mapbox.com/styles/v1/group13-iv/ckujan42s62yl17qm7o5ve1hg.html?title=false&access_token=pk.eyJ1IjoiZ3JvdXAxMy1pdiIsImEiOiJja3VqYWdxMHgxZXY5MnZ0NGR6MHcyajcyIn0.JYGckVyFytw5iPy6um8vcw&zoomwheel=false#14.32/-37.81468/144.96289" title="GEOM90007-Group Assignment" style="border:none;"></iframe>
+<iframe width='100%' height='400px' src="https://api.mapbox.com/styles/v1/group13-iv/ckujan42s62yl17qm7o5ve1hg.html?title=copy&access_token=pk.eyJ1IjoiZ3JvdXAxMy1pdiIsImEiOiJja3VqYWdxMHgxZXY5MnZ0NGR6MHcyajcyIn0.JYGckVyFytw5iPy6um8vcw&zoomwheel=true&fresh=true#14.32/-37.81468/144.96289" title="GEOM90007-Group Assignment" style="border:none;"></iframe>
 `
