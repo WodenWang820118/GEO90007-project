41d1f3db8c9adcf7507135253a5a1446aaad93e2
GuanXin <48168793+WodenWang820118@users.noreply.github.com>
2021-10-13 14:19:13 +0800

    Example writing code directly on github
    Description

--- scripts/mapbox/map.js
+++ scripts/mapbox/map.js
@@ -3,3 +3,4 @@ map.innerHTML =
 `
 <iframe width='100%' height='1000px' src="https://api.mapbox.com/styles/v1/group13-iv/ckujan42s62yl17qm7o5ve1hg.html?title=copy&access_token=pk.eyJ1IjoiZ3JvdXAxMy1pdiIsImEiOiJja3VqYWdxMHgxZXY5MnZ0NGR6MHcyajcyIn0.JYGckVyFytw5iPy6um8vcw&zoomwheel=true&fresh=true#14.32/-37.81468/144.96289" title="GEOM90007-Group Assignment" style="border:none;"></iframe>
 `
+<h1>Example</h1>
