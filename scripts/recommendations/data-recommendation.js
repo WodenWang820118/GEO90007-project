1934c42e01b4d5a285677e7c2c3ef43473989098
GuanXin Wang <Alex0972112871@gmail.com>
2021-10-20 13:01:12 +0800

    recommendation picture; newletter arrow

---
+++ scripts/recommendations/data-recommendation.js
@@ -0,0 +1,20 @@
+var data = [
+  {
+    "image": "Industry Beans",
+    "name": "Industry Beans",
+    "location" : "3/62 Rose St. Fitzroy",
+    "dietary Options": "Vegetarian-Friendly Gluten Free Options, Vegan Options"
+  },
+  {
+    "image": "Tall Timber",
+    "name": "Tall Timber",
+    "location" : "60 Commercial Rd, Prahran",
+    "dietary Options": "Vegetarian-Friendly, Vegan Options, Gluten Free Options"
+  },
+  {
+    "image": "Fourth Chapter",
+    "name": "Fourth Chapter",
+    "location" : "385 High St, Prahran",
+    "dietary Options": "Vegan Options"
+  },
+]
--- scripts/recommendations/recommendations.js
+++ scripts/recommendations/recommendations.js
@@ -1,26 +1,8 @@
 const recommendations = document.getElementById('recommendations')
 
-var data = [
-  {
-    "image": "Industry Beans",
-    "name": "Industry Beans",
-    "location" : "3/62 Rose St. Fitzroy",
-    "dietary Options": "Vegetarian-Friendly Gluten Free Options, Vegan Options"
-  },
-  {
-    "image": "Tall Timber",
-    "name": "Tall Timber",
-    "location" : "60 Commercial Rd, Prahran",
-    "dietary Options": "Vegetarian-Friendly, Vegan Options, Gluten Free Options"
-  },
-  {
-    "image": "Fourth Chapter",
-    "name": "Fourth Chapter",
-    "location" : "385 High St, Prahran",
-    "dietary Options": "Vegan Options"
-  },
-]
-
+// default recommendations
+// prototype; information not real
+// data from the global variable from data-recommendation.js
 data.forEach(element => {
   let name = element.name
   let location = element.location
@@ -44,14 +26,70 @@ data.forEach(element => {
   `
 });
 
-// load up external CSS file
-var link = document.createElement('link');
+/**
+ * The function get the event from the area button
+ * and bring the recommendation dynamically
+ * @param {*} e 
+ */
+function setRecommend(e) {
+  console.log(e.currentTarget.id)
+  let area = e.currentTarget.id
+  // reset the innerHTML; list of pictures
+  recommendations.innerHTML=''
+  let lst = []
 
-// set properties of link tag
-// the CSS path must be the relative path of the root directory
-link.href = 'scripts/recommendations/recommendations.css';
-link.rel = 'stylesheet';
-link.type = 'text/css';
+  if (area == 'central') {
+    lst = ['EauDeVie', 'Maha', 'TipoOO']
+  }
+  else if (area == 'east') {
+    lst = ['LuneCroissant', 'RicePaperScissors', 'VueDeMonde']
+  }
+  else if (area == 'north') {
+    lst = ['Dexter', 'HalfMoonCafe', 'LaksaKing']
+  }
+  else if (area == 'south') {
+    lst = ['Amaru', 'attica', 'LEFTFIELD']
+  }
+  else if (area == 'west') {
+    lst = ['Fourth Chapter', 'Industry Beans', 'Tall Timber']
+  }
 
-// append link element to html
-document.getElementById("recommendations").appendChild(link);
+  lst.forEach(pic_name => {
+    recommendations.innerHTML+=
+    `
+      <div class="container card">
+        <div class="row">
+          <div class="col-lg-6">
+            <img src="assets/recommendations/${area}/${pic_name}.jpg" class="img-fluid" style="width:600px;">
+          </div>
+          <div class="col-lg-6">
+            <p> <strong>Industry Beans</strong> </p>
+            <p> <strong>Location:</strong> 3/62 Rose St. Fitzroy</p>
+            <p> <strong>Dietary Options:</strong> Vegetarian-Friendly Gluten Free Options, Vegan Options</p>
+          </div>
+        </div>
+      </div>
+      </br>
+    `
+  })
+  loadCSS()
+}
+
+/**
+ * The function to reload the CSS dynamically
+ */
+function loadCSS() {
+  // load up external CSS file
+  var link = document.createElement('link');
+
+  // set properties of link tag
+  // the CSS path must be the relative path of the root directory
+  link.href = 'scripts/recommendations/recommendations.css';
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+
+  // append link element to html
+  document.getElementById("recommendations").appendChild(link);
+}
+
+loadCSS()
